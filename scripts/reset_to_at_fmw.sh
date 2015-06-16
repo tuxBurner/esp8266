e01426f72a03d26d3594fbfda09de87560b5601f
Sebastian Hardt <tuxburner@boggo.de>
2015-06-16 22:53:37 +0200

    Added a possibility to repait the nodemce

---
+++ scripts/reset_to_at_fmw.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+################################################
+### Flashes the nodemcu firmware on the esp ###
+###############################################
+
+# include the env vars
+. ./envVars.sh
+
+########################################
+### Downloads the current firmeware ###
+#######################################
+function downloadFirmWare() {
+  FMW_DEST_FILE=$DATA_DIR"/firmware.bin"
+  if [ -f $FMW_DEST_FILE ]
+  then
+    echo "Found $FMW_DEST_FILE deleting it"
+    rm $FMW_DEST_FILE
+  fi
+  cp "v0.9.5.2 AT Firmware.bin" $FMW_DEST_FILE
+}
+
+#########################################
+### Checkout the esptool from github ###
+########################################
+function getOrUpdateEspTool() {
+  cloneOrUpdateGit "https://github.com/themadinventor/esptool.git" $ESP_TOOL_DIR
+}
+
+##########################################
+### Flashes the ESP wit the firmeware ###
+#########################################
+function flashESP() {
+  cd $ESP_TOOL_DIR
+  sudo python esptool.py --port $ESP_PORT  write_flash 0x00000 ../firmware.bin
+  cd $BASE_DIR
+}
+
+downloadFirmWare
+#getOrUpdateEspTool
+flashESP
