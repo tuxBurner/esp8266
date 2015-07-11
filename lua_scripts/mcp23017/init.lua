6f4b77a8a8a704a0bede70602a1be77c801704fa
Sebastian Hardt <tuxburner@boggo.de>
2015-07-11 20:32:17 +0200

    init lua

--- lua_scripts/mcp23017/init.lua
+++ lua_scripts/mcp23017/init.lua
@@ -1,38 +1,17 @@
+
 -- make ist fast :)
 --uart.setup(0,115200,8,0,1,1,PERMANENT)
 -- mcp starts here
 mcp = require("mcp23017")
 mcp.init(3,4)
-
---mcp.setPin(8,1)
---mcp.setPin(9,1)
-local compileAndRemoveIfNeeded = function(f)
-   if file.open(f) then
-      file.close()
-      print('Compiling:', f)
-      node.compile(f)
-      file.remove(f)
-      collectgarbage()
-   end
-end
-local serverFiles = {'httpserver.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
-for i, f in ipairs(serverFiles) do compileAndRemoveIfNeeded(f) end
-compileAndRemoveIfNeeded = nil
-serverFiles = nil
-collectgarbage()
-
-
-function test()
-  print("started the esp")
-  dofile("httpserver.lc")(80)
-  print('heap: ',node.heap())
+mcp.setUpPin(9,1,1)
+-- set pin to input and pullup
+print('Okay')
+function showButtons()
+  -- negate the output
+  local state = mcp.getPin(9)
+  mcp.setPin(1,state)
   collectgarbage()
+  tmr.wdclr()
 end
-
-
-wifiCfg=require('wifiCfg')
-wifiCfg.setup(test)
-wifiCfg=nil
-collectgarbage()
-
-
+tmr.alarm(0,150,1,showButtons) -- run showButtons() every 2 seconds
