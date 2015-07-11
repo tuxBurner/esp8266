6789772bf921232e9e979d98e5e80f2089150c16
Sebastian Hardt <tuxburner@boggo.de>
2015-07-11 20:26:18 +0200

    Getting there

--- lua_scripts/iot/http_index.lua
+++ lua_scripts/iot/http_index.lua
@@ -4,11 +4,11 @@ return function(cn,params)
   for name, value in pairs(params) do
    mcp.setPin(tonumber(name),tonumber(value))
   end
-  cn:send(", mcpInfo : {" )
+  cn:send(", \"mcpInfo\" : {" )
   local com = ""
   for i=1,16 do 
     local state = mcp.getPin(i)
-    cn:send(com..i..":"..mcp.getPin(i))
+    cn:send(com.."\""..i.."\":"..mcp.getPin(i))
     com=","
   end
   cn:send("}")
--- lua_scripts/mcp23017/init.lua
+++ lua_scripts/mcp23017/init.lua
@@ -3,12 +3,36 @@
 -- mcp starts here
 mcp = require("mcp23017")
 mcp.init(3,4)
-mcp.setUpPin(9,1,1)
-function showButtons()
-  -- negate the output
-  local state = mcp.getPin(9)
-  mcp.setPin(1,state)
-  collectgarbage()
-  tmr.wdclr()
+
+--mcp.setPin(8,1)
+--mcp.setPin(9,1)
+local compileAndRemoveIfNeeded = function(f)
+   if file.open(f) then
+      file.close()
+      print('Compiling:', f)
+      node.compile(f)
+      file.remove(f)
+      collectgarbage()
+   end
 end
-tmr.alarm(0,150,1,showButtons) -- run showButtons() every 2 seconds
+local serverFiles = {'httpserver.lua', 'httpserver-request.lua', 'httpserver-static.lua', 'httpserver-header.lua', 'httpserver-error.lua'}
+for i, f in ipairs(serverFiles) do compileAndRemoveIfNeeded(f) end
+compileAndRemoveIfNeeded = nil
+serverFiles = nil
+collectgarbage()
+
+
+function test()
+  print("started the esp")
+  dofile("httpserver.lc")(80)
+  print('heap: ',node.heap())
+  collectgarbage()
+end
+
+
+wifiCfg=require('wifiCfg')
+wifiCfg.setup(test)
+wifiCfg=nil
+collectgarbage()
+
+
